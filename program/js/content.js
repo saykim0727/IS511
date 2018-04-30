dabfe766bf22dc7bcaa4ba6f6ac68adf651ff308
ssssragon <s.jeong@kaist.ac.kr>
2018-04-30 17:10:20 +0900

    Worker capturing Be able to log worker-generation, make list of generated workers, terminate all running workers.

---
+++ program/js/content.js
@@ -0,0 +1,29 @@
+var injector = function(){
+	if(arguments.length == 2) {
+		var path = arguments[0];
+		var cb = arguments[1];
+
+		var script = document.createElement('script');
+		
+			script.src = chrome.extension.getURL(path);
+		script.onload = function() {
+			this.remove();
+			if(cb !== undefined && typeof cb == 'function') {
+				cb();
+			}
+		};
+		
+		(document.head||document.documentElement).appendChild(script);
+	
+	} else if (arguments.length == 1) {
+		var text = arguments[0];
+
+		var script = document.createElement('script');
+		script.textContent = text;
+		(document.head||document.documentElement).appendChild(script);
+		
+		script.remove();
+	};
+};
+
+injector("js/customworker.js",null);
---
+++ program/js/customworker.js
@@ -0,0 +1,28 @@
+var __workers = [];
+__workers.list = [];
+__workers.terminate = function() {
+	wlist.forEach(function (value) {
+		if(typeof value == "object" && value.isRunning == true)
+			value.terminate();
+	});
+};
+__workers.getNumber = function() {
+	return this.list.length;
+}
+
+var __WORKER = Worker;
+
+Worker = function(args) {
+	console.log("A worker is initialzied");
+	var tempWK = new __WORKER(args);
+	tempWK.__argument = args;
+	tempWK.isRunning = true;
+	__workers.list.push(tempWK);
+
+	tempWK.__terminate = tempWK.terminate;
+	tempWK.terminate = function() {
+		this.isRunning = false;
+		tempWK.__terminate.apply(this, arguments);
+	}
+	return tempWK;
+}
