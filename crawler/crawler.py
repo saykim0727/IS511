10bf9219bcc4a60547d131610090677dfefabedc
saykim0727 <saykim0727@kaist.ac.kr>
2018-05-02 20:34:26 +0900

    Update crawler.py

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -1,12 +1,26 @@
 import requests
 import re
 from bs4 import BeautifulSoup
+import signal
+import sys
+import os
 url_file = "live.txt"
 req=""
 html=""
 count=0
 result=""
 head="['"+'"]'
+
+def handler(signum,frame):
+	global fff
+	global ff
+	fff.close()
+	ff.close()
+	os._exit(0)
+
+signal.signal(signal.SIGTSTP,handler)
+ff = open("result.txt","a+")
+fff = open("domain.txt","a+")
 with open(url_file,"r") as f:
         while 1:
                 url = f.readline()
@@ -21,39 +35,37 @@ with open(url_file,"r") as f:
 	                html = req.text
         	        soup = BeautifulSoup(html, 'html.parser')
                 	temp=str(soup)
-			ff = open("result.txt","a+")
 			try: 
 	        	        result = re.split('key\s*=\s*'+head,temp)
         	        	result = re.split(head,result[1])
-				pval = "%40s   %s\n"%(url, result[0])
-				print pval
+				pval = "%-80s   %-s\n"%(url, result[0])
+				print pval[:-1]
 	        	        ff.write(pval)
 			except:
 				check+=1
 			try:
 	        	        result = re.split("Anonymous\("+head,temp)
         	        	result = re.split(head,result[1])
-				pval = "%40s   %s\n"%(url, result[0])
-				print pval
+				pval = "%-80s   %-s\n"%(url, result[0])
+				print pval[:-1]
 	        	        ff.write(pval)
 			except:
 				check+=1
 			try:
 	        	        result = re.split('User\('+head,temp)
         	        	result = re.split(head,result[1])
-				pval = "%40s   %s\n"%(url, result[0])
-				print pval
+				pval = "%-80s   %-s\n"%(url, result[0])
+				print pval[:-1]
 	        	        ff.write(pval)
 			except:
 				check+=1
 			if check==3:
-				print url+"   haven't value"
-			ff.close()
-			fff = open("domain.txt","a+")
+				pval = "%-80s   %-s\n"%(url,"haven't value")
+				print pval[:-1]
 			fff.write(url+"\n")
-			fff.close()
 		except:
-			print url+"   domain not valid"
+			pval = "%-80s   %-s\n"%(url,"domain not valied")
+			print pval[:-1]
 			continue
 
 print count
